37b75ce5f3208fcd29bb9ac0f51e5a9477399ac4
4b1g0011 <4b1g0011@office.stust.edu.tw>
2023-04-16 22:22:00 +0800

    #include <iostream> #include <string> #include <algorithm> // 包含 transform() 函式的標頭檔
    using namespace std;
    
    int main() {
     while (true) {
      string str[2];
      getline(cin, str[0]); // 讀取使用者輸入的字串
      string dict = "~!@#$%^&*()_++`1234567890-=={}||qwertyuiop[]\\:""asdfghjkl;''<>??zxcvbnm,.//";
      transform(str[0].begin(), str[0].end(), str[0].begin(), ::tolower); // 將字串中的英文字母轉換為小寫
      for (int i = 0; i < str[0].length(); ++i) {
       for (int j = 0; j < dict.length(); ++j) {
        if (str[0][i] == dict[j]) { // 搜尋字典中對應的字元
         cout << dict[j + 1]; // 輸出對應的字元
         break;
        }
       }
      }
      cout << endl; // 換行
     }
     return 0;
    }

--- ConsoleApplication6.cpp
+++ ConsoleApplication6.cpp
@@ -1,41 +1,23 @@
-﻿#include <iostream> // 輸入輸出流庫  
-#include <string> // 字串庫  
-
+﻿#include<iostream>  
+#include<string>  
 using namespace std;
-
+int appearNum(string& s, int p, string& l);
 int main()
 {
-    string str; // 定義一個字串變數  
-    int count = 1; // 紀錄單字數量的變數初始化為1，因為第一個單字之前沒有空格  
-    int arr[26] = { 0 }; // 用於記錄大寫字母出現次數的陣列  
-    int arr1[26] = { 0 };// 用於記錄小寫字母出現次數的陣列  
-    getline(cin, str); // 從標準輸入讀取一行字串  
-    // 遍歷字串中的每一個字元  
-    for (int i = 0; i < str.length(); i++)
+    string inputS, inputL, save;
+
+    cin >> inputS >> inputL;
+    cout << appearNum(inputS, 0, inputL) << endl;
+    return 0;
+}
+int appearNum(string& s, int p, string& l)//字串遞迴(檢索字串，起始位置，被檢索字串)   
+{
+    int a = 0;
+    if (p > l.length())return a = 0;//若偏移長度大於被檢索字串長度則回傳0   
+    for (int i = 0; i < s.length(); i++)//依檢索字串長度進行迴圈   
     {
-        // 如果是大寫字母，將對應字母計數陣列中的相應元素加1  
-        if (str[i] >= 'A' && str[i] <= 'Z')
-            arr[str[i] - 'A']++;
-        // 如果是小寫字母，將對應字母計數陣列中的相應元素加1  
-        else if (str[i] >= 'a' && str[i] <= 'z')
-            arr1[str[i] - 'a']++;
-        // 如果是空格，說明當前字元是一個單字的分隔符，單字數量加1  
-        if (str[i] == ' ')
-            count++;
+        if (l[i + p] == s[i])continue;//若比對符合，則持續比對至完全符合   
+        else return a += appearNum(s, p + 1, l);//比對不符回傳當前符合次數   
     }
-
-    // 輸出單字數量  
-    cout << count << endl;
-
-    // 遍歷兩個計數陣列，輸出每個字母出現的次數  
-    for (int j = 0; j < 26; j++)
-    {
-        if (arr[j] != 0) // 如果大寫字母計數陣列中第j個元素不為0，說明字母('A'+j)出現過  
-            cout << (char)('a' + j) << " : " << arr[j] << endl; // 輸出該字母及其出現次數  
-        else if (arr1[j] != 0) // 否則，如果小寫字母計數陣列中第j個元素不為0，說明字母('a'+j)出現過  
-            cout << (char)('a' + j) << " : " << arr1[j] << endl; // 輸出該字母及其出現次數  
-    }
-
-    return 0; // 程序結束  
-
+    a++; return a += appearNum(s, p + 1, l);//迴圈完畢表示比對完全符合，計次後往右偏移再測試   
 }
